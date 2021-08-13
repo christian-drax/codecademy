0efa187be7c1c0834fd0fb4475aebb6d14b6edb7
christian-drax <73065554+christian-drax@users.noreply.github.com>
2021-08-13 20:01:17 +0100

    letter.cpp

---
+++ letter.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+
+int main(){
+
+    std::cout << "Dear Fam,\n";
+    std::cout << "I will learn C++ language befpre the begining of the next academic session.\n";
+
+    std::cout << "                                             Yours Sincerely\n";
+    std::cout << "                                                  13/08/2021\n";
+    std::cout << "                                            Onuoha Christian\n";
+}
